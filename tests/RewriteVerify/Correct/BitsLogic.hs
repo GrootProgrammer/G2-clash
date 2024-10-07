28251a18e50671a340fe79bafdbfffa32d2c67a2
r.t.groot@student.utwente.nl <r.t.groot@student.utwente.nl>
2024-10-07 15:14:57 +0200

    add test for morgan and

--- tests/RewriteVerify/Correct/BitsLogic.hs
+++ tests/RewriteVerify/Correct/BitsLogic.hs
@@ -7,6 +7,13 @@ left_morgan_or a b = complement (a .|. b)
 right_morgan_or :: Int -> Int -> Int
 right_morgan_or a b = ((complement a) .&. (complement b))
 
+
+left_morgan_and :: Int -> Int -> Int
+left_morgan_and a b = complement (a .&. b)
+right_morgan_and :: Int -> Int -> Int
+right_morgan_and a b = ((complement a) .|. (complement b))
+
 {-# RULES 
-    "morgans law" forall a b. left_morgan_or a b = right_morgan_or a b 
+"morgans law or" forall a b. left_morgan_or a b = right_morgan_or a b 
+"morgans law and" forall a b. left_morgan_and a b = right_morgan_and a b 
 #-}
